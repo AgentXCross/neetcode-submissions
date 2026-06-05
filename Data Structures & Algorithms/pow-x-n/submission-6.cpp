7c9e7ab57df54ba48454104ec047d7a1e36ffa14
Michael Liu <160261168+AgentXCross@users.noreply.github.com>
2026-06-04 22:23:38 -0400

    Add: pow-x-n - submission-6

---
+++ Data Structures & Algorithms/pow-x-n/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double result = 1.00;
+        long long power = n;
+
+        if (power == 0) {
+            return 1;
+        }
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        while (power > 0) {
+            if (power % 2 == 1) {
+                result *= x;
+            } 
+            x *= x;
+            power /= 2;
+        }
+        return result;
+    }
+};
