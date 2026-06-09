588028ec988701a3c169173b709da74e4c85874c
Michael Liu <160261168+AgentXCross@users.noreply.github.com>
2026-06-08 21:41:24 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+private:
+    vector<int> stk;
+    vector<int> min_stk;
+
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        stk.push_back(val);
+
+        if (min_stk.empty()) {
+            min_stk.push_back(val);
+        } else {
+            min_stk.push_back(min(val, min_stk.back()));
+        }
+    }
+    
+    void pop() {
+        stk.pop_back();
+        min_stk.pop_back();
+    }
+    
+    int top() {
+        return stk.back();
+    }
+    
+    int getMin() {
+        return min_stk.back();
+    }
+};
