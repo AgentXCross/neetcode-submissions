6963e6cd8272e7edb5ca1da9df1e0e935ab13fff
Michael Liu <160261168+AgentXCross@users.noreply.github.com>
2026-09-11 19:43:05 -0400

    Add: is-palindrome - submission-18

---
+++ Data Structures & Algorithms/is-palindrome/submission-18.cpp
@@ -0,0 +1,55 @@
+class Solution {
+private:
+    bool is_alphanumeric(char c) {
+        if (c >= '0' && c <= '9' || 
+            c >= 'a' && c <= 'z' ||
+            c >= 'A' && c <= 'Z'
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    char lower(char c) {
+        if (c >= '0' && c <= '9') {
+            return c;
+        } else if (c >= 'a' && c <= 'z') {
+            return c;
+        } else {
+            return c - 'A' + 'a';
+        }
+    }
+    
+public:
+    bool isPalindrome(std::string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (is_alphanumeric(s[l]) && is_alphanumeric(s[r])) {
+                char left = lower(s[l]);
+                char right = lower(s[r]);
+
+                if (left != right) {
+                    return false;
+                }
+
+                l++;
+                r--;
+
+                continue;
+            }
+
+            while (!is_alphanumeric(s[l]) && l <= s.size() - 1) {
+                l++;
+            }
+
+            while(!is_alphanumeric(s[r]) && r >= 0) {
+                r--;
+            }
+        }
+
+        return true;
+    }
+};
